190035d8217bb548447a4fa0a0014c075dae63f5
arslan <103488882+arslan11-codes@users.noreply.github.com>
2025-05-07 04:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+# main.py
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "🚀 Hello from GCP with CI/CD Pipeline!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+def hello():
+    return "Hello, World from Flask on Cloud Run!"
